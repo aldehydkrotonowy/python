cb0fe7b0cc2f2de3f9a302da514021e589f3f158
Norbert Gajda <norbert.gajda@gmail.com>
2019-08-19 21:33:18 +0200

    a

--- scikit-learn/lesson2.py
+++ scikit-learn/lesson2.py
@@ -1,26 +1,55 @@
-import numpy as np
 from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+import numpy as np
 print(__doc__)
+
+#
+# Demonstration of k-means assumptions
+# from: https://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_assumptions.html#sphx-glr-download-auto-examples-cluster-plot-kmeans-assumptions-py
+# Author: Phil Roth <mr.phil.roth@gmail.com>
+# License: BSD 3 clause
 
 
 plt.figure(figsize=(12, 12))
+
 n_samples = 1500
 random_state = 170
-
-# Generate isotropic Gaussian blobs for clustering
 X, y = make_blobs(n_samples=n_samples, random_state=random_state)
-print(X)
-print(X.shape)  # The generated samples
-print(y)  # The integer labels for cluster membership of each sample
-y_pred = KMeans(n_clusters=2, random_state=random_state).fit_predict(X)
 
-print(y_pred)
+# Incorrect number of clusters
+y_pred = KMeans(n_clusters=2, random_state=random_state).fit_predict(X)
 
 plt.subplot(221)
 plt.scatter(X[:, 0], X[:, 1], c=y_pred)
-plt.title('incorrect number of blobs')
+plt.title("Incorrect Number of Blobs")
 
+# Anisotropicly distributed data
+transformation = [[0.60834549, -0.63667341], [-0.40887718, 0.85253229]]
+X_aniso = np.dot(X, transformation)
+y_pred = KMeans(n_clusters=3, random_state=random_state).fit_predict(X_aniso)
+
+plt.subplot(222)
+plt.scatter(X_aniso[:, 0], X_aniso[:, 1], c=y_pred)
+plt.title("Anisotropicly Distributed Blobs")
+
+# Different variance
+X_varied, y_varied = make_blobs(n_samples=n_samples,
+                                cluster_std=[1.0, 2.5, 0.5],
+                                random_state=random_state)
+y_pred = KMeans(n_clusters=3, random_state=random_state).fit_predict(X_varied)
+
+plt.subplot(223)
+plt.scatter(X_varied[:, 0], X_varied[:, 1], c=y_pred)
+plt.title("Unequal Variance")
+
+# Unevenly sized blobs
+X_filtered = np.vstack((X[y == 0][:500], X[y == 1][:100], X[y == 2][:10]))
+y_pred = KMeans(n_clusters=3,
+                random_state=random_state).fit_predict(X_filtered)
+
+plt.subplot(224)
+plt.scatter(X_filtered[:, 0], X_filtered[:, 1], c=y_pred)
+plt.title("Unevenly Sized Blobs")
 
 plt.show()
